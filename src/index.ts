8ce9856e04915fc032e5675037d9eeb47e98b416
Shobhit1011 <shobhitcoolsinghal@gmail.com>
2023-07-14 22:56:39 +0530

    feat: :fire: Added apidocs

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,13 @@ import express, { Express, NextFunction, Request, Response } from "express";
 import * as dotenv from "dotenv";
 import * as mongoose from "mongoose";
 import * as bodyParser from "body-parser";
+import path from "path";
 import { logger } from "./logger";
 dotenv.config();
 import config from "./config";
 import { errorHandler } from "./errorHandler";
 import UserRoutes from "./routes/userRoutes";
 import { NotFoundError } from "./errors";
-
 
 console.log = (...args) => { logger.info(args); };
 console.error = (...args) => { logger.error(args); };
@@ -28,9 +28,11 @@ app.listen(config.get('port'), () => {
 
 // request body parsing.
 app.use(bodyParser.json());
-
+// apidocs.
+app.use("/apidocs", express.static(path.join(__dirname, 'docs')));
 app.use("/users", UserRoutes);
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((_req: Request, _res: Response, _next: NextFunction) => {
     throw new NotFoundError({});
 });
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -7,6 +7,26 @@ import { UserValidators } from "../validators";
 
 const router: core.Router = Router();
 
+/**
+ * @api {post} /users Create User
+ * @apiName Create User
+ * @apiGroup User
+ *
+ * @apiBody {String} email User email.
+ * @apiBody {String} phoneNo User phoneNo.
+ * @apiBody {String} name User name.
+ * 
+ * @apiSuccess {Number} status Response status.
+ * @apiSuccess {String} customCode Response customCode.
+ * @apiSuccess {String} message Success message.
+ * @apiSuccess {Object} data User Info.
+ * @apiSuccess {String} data.userId User's userId.
+ * @apiSuccess {String} data.email User email.
+ * @apiSuccess {String} data.phoneNo User phoneNo.
+ * @apiSuccess {String} data.name User name.
+ * @apiSuccess {Date} data.createdAt User created date.
+ * @apiSuccess {Date} data.updatedAt User updated date.
+ */
 router.post("/", UserValidators.createUserValidator ,async (req: Request, res: Response, next: NextFunction) => {
     try {
         const _inst = new UserService();
